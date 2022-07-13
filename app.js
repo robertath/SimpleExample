3ab7c82b42560ce1fd450f425ca27bac96ad3b82
Roberta Tagliari Hoffmann <45298958+robertath@users.noreply.github.com>
2022-07-13 17:51:30 +0100

    Fixing the app

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Running hame management system")
+console.log("Created database connected")
+console.log("New update to check")
+console.log("New message to check") 
+console.log("Created a contact page") 
